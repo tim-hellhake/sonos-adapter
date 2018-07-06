2478722bed2bbf1c6abc0d7f006b96b97a749d15
Martin Giger <martin@humanoids.be>
2018-07-06 21:22:57 +0200

    Support more properties and use property caps

--- speaker.js
+++ speaker.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Property = require('./property');
+const ReadonlyProperty = require('./readonly-property');
 const { Sonos } = require("sonos");
 
 let Device, Constants;
@@ -40,36 +41,77 @@ class Speaker extends Device {
         this.device = device;
         this.name = ip;
         this.type = Constants.THING_TYPE_UNKNOWN_THING;
+        this["@type"] = [ "Speaker", "MediaPlayer" ];
+
         this.properties.set('volume', new Property(this, 'volume', {
+            label: "Volume",
             type: 'number',
-            unit: 'percent'
+            unit: 'percent',
+            "@type": "LevelProperty"
         }, 1.0));
         this.properties.set('playing', new Property(this, 'playing', {
-            type: 'boolean'
+            label: "Play/Pause",
+            type: 'boolean',
+            "@type": "BooleanProperty"
         }, false));
         this.properties.set('shuffle', new Property(this, 'shuffle', {
-            type: 'boolean'
+            label: "Shuffle",
+            type: 'boolean',
+            "@type": "BooleanProperty"
         }, false));
         this.properties.set('repeat', new Property(this, 'repeat', {
-            type: 'boolean'
+            label: "Repeat",
+            type: 'boolean',
+            "@type": "BooleanProperty"
         }, false));
+        this.properties.set('crossfade', new Property(this, 'crossfade', {
+            label: "Crossfade",
+            type: "boolean",
+            "@type": "BooleanProperty"
+        }, false));
+        this.properties.set('track', new ReadonlyProperty(this, 'track', {
+            label: "Track",
+            type: "string",
+            "@type": "StringProperty"
+        }, ''));
+        this.properties.set('album', new ReadonlyProperty(this, 'album', {
+            label: "Album",
+            type: "string",
+            "@type": "StringProperty"
+        }, ''));
+        this.properties.set('artist', new ReadonlyProperty(this, 'artist', {
+            label: "Artist",
+            type: "string",
+            "@type": "StringProperty"
+        }, ''));
+        this.properties.set('progress', new Property(this, 'progress', {
+            label: "Progress",
+            type: "number",
+            "@type": "LevelProperty"
+        }, 0));
+        this.currentDuration = 0;
+        this.currentPosition = 0;
 
-        //TODO crossfade mode
         //TODO eq
         //TODO loudness
         //TODO repeat one
         //TODO balance for stereo pairs
         //TODO handle fixed volume setting changing
         //TODO actions? Like clear queue, stop
-        //TODO fields like current track, queue size
+        //TODO fields like current track (album art), queue size
+        //TODO queue track position property
         //TODO property for queue size/stopped?
         //TODO playback state when grouped
+        //TODO play line-in
+        //TODO action to play notification (needs file input and has a toggle for where to play)
         // Useful list of things: https://github.com/SoCo/SoCo/wiki/Sonos-UPnP-Services-and-Functions
 
         this.addAction('next', {
+            label: "Next",
             description: "Skip current track and start playing next track in the queue"
         });
         this.addAction('prev', {
+            label: "Previous",
             description: "Play previous track in the queue"
         });
 
@@ -86,7 +128,7 @@ class Speaker extends Device {
             const hasFixedVolume = await this.getFixedVolume();
             shouldGetVolume = !hasFixedVolume;
             if(hasFixedVolume) {
-                this.properties.delete('volume');
+                //TODO make property read-only
             }
         }
         if(shouldGetVolume) {
@@ -97,11 +139,29 @@ class Speaker extends Device {
         const state = await this.device.getCurrentState();
         this.updateProp('playing', state === 'playing');
 
+        const currentTrack = await this.device.currentTrack();
+        if(currentTrack.duration > 0) {
+            this.updateProp('track', currentTrack.title);
+            this.updateProp('album', currentTrack.album);
+            this.updateProp('artist', currentTrack.artist);
+            this.updateProp('progress', (currentTrack.position / currentTrack.duration) * 100);
+            this.currentDuration = currentTrack.duration;
+            this.currentPosition = currentTrack.position;
+            if(state === 'playing') {
+                this.progressInterval = setInterval(() => this.updateProgress(), 1000);
+            }
+        }
+        //TODO current track change event
+
         const mode = await this.device.getPlayMode();
         this.updatePlayMode(mode);
 
+        const crossFade = await this.getCrossfadeMode();
+        this.updateProp('crossfade', crossFade);
+
         const topo = await this.device.getTopology();
         const groupDetails = {
+            label: "Group/Ungroup",
             description: "Group Sonos players",
             input: {
                 type: "object",
@@ -122,16 +182,54 @@ class Speaker extends Device {
         this.addAction('group', groupDetails);
 
         this.device.on('PlayState', (state) => {
-            this.updateProp('playing', state === 'playing');
+            const playing = state === 'playing';
+            this.updateProp('playing', playing);
+            if(playing && !this.progressInterval && this.currentDuration) {
+                this.progressInterval = setInterval(() => this.updateProgress(), 1000);
+            }
+            else if(!playing && this.progressInterval) {
+                this.clearProgress();
+            }
         });
 
         this.device.on('PlaybackStopped', () => {
             this.updateProp('playing', false);
+            this.updateProp('progress', 0);
+            this.clearProgress();
         });
 
         this.device.on('AVTransport', (newValue) => {
             const mode = newValue.CurrentPlayMode;
             this.updatePlayMode(mode);
+            this.updateProp('crossfade', newValue.CurrentCrossfadeMode != '0');
+            if(newValue.CurrentTrackMetaDataParsed) {
+                this.updateProp('track', newValue.CurrentTrackMetaDataParsed.title);
+                this.updateProp('artist', newValue.CurrentTrackMetaDataParsed.artist);
+                this.updateProp('album', newValue.CurrentTrackMetaDataParsed.album);
+            }
+            else {
+                this.updateProp('track', '');
+                this.updateProp('artist', '');
+                this.updateProp('album', '');
+            }
+            this.device.currentTrack().then((currentTrack) => {
+                this.currentDuration = currentTrack.duration;
+                this.currentPosition = currentTrack.position;
+                if(this.currentDuration != 0) {
+                    this.updateProp('progress', (currentTrack.position / currentTrack.duration) * 100);
+                    const isPlaying = this.findProperty('playing').value;
+                    if(isPlaying && !this.progressInterval) {
+                        this.progressInterval = setInterval(() => this.updateProgress(), 1000);
+                    }
+                    else if(!isPlaying) {
+                        this.clearProgress();
+                    }
+                }
+                else {
+                    this.updateProp('progress', 0);
+                    this.clearProgress();
+                }
+            });
         });
 
         //TODO update group action
@@ -142,6 +240,7 @@ class Speaker extends Device {
                 this.updateProp('volume', volume);
             });
         }
+        //TODO else add listener for fixed volume to be disabled
     }
 
     get renderingControl() {
@@ -151,11 +250,11 @@ class Speaker extends Device {
         return this._renderingControl;
     }
 
-    get groupManagement() {
-        if(!this._groupManagement) {
-            this._groupManagement = this.device.getGroupManagementService();
+    get avTransport() {
+        if(!this._avTransport) {
+            this._avTransport = this.device.avTransportService();
         }
-        return this._groupManagement;
+        return this._avTransport;
     }
 
     async getSupportsFixedVolume() {
@@ -168,13 +267,30 @@ class Speaker extends Device {
         return response.CurrentFixed != '0';
     }
 
+    async getCrossfadeMode() {
+        const response = await this.avTransport.GetCrossfadeMode();
+        return response.CurrrentCrossfadeMode != '0';
+    }
+
     updatePlayMode(mode) {
         this.updateProp('shuffle', mode && mode.startsWith('SHUFFLE'));
         this.updateProp('repeat', mode === 'REPEAT_ALL' || mode === 'SHUFFLE');
     }
 
+    updateProgress() {
+        this.currentPosition += 1;
+        this.updateProp('progress', (this.currentPosition / this.currentDuration) * 100);
+    }
+
+    clearProgress() {
+        if(this.progressInterval) {
+            clearInterval(this.progressInterval);
+            this.progressInterval = undefined;
+        }
+    }
+
     updateProp(propertyName, value) {
-        const property= this.properties.get(propertyName);
+        const property = this.findProperty(propertyName);
         if(property.value !== value) {
             property.setCachedValue(value);
             super.notifyPropertyChanged(property);
@@ -193,6 +309,7 @@ class Speaker extends Device {
                 }
             break;
             case 'volume':
+                //TODO reject if volume is fixed.
                 await this.device.setVolume(newValue);
             break;
             case 'shuffle':
@@ -203,6 +320,19 @@ class Speaker extends Device {
                         this.properties.get('repeat').value
                     )
                 );
+            break;
+            case 'crossfade':
+                await this.avTransport.SetCrossfadeMode({ InstanceID: 0, CrossfadeMode: newValue });
+            break;
+            case 'progress':
+                if(this.currentDuration > 0) {
+                    const newPosition = Math.floor((newValue / 100) * this.currentDuration);
+                    await this.device.seek(newPosition);
+                    this.currentPosition = newPosition;
+                }
+                else {
+                    throw "Can't change progress without track";
+                }
             break;
         }
         super.notifyPropertyChanged(property);
@@ -222,6 +352,7 @@ class Speaker extends Device {
             break;
             case "group":
                 action.start();
+                //TODO only execute if the new group config is different from the current one.
                 await this.device.leaveGroup();
                 const topo = await this.device.getTopology();
                 for(const input in action.input) {
