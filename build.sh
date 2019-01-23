59183859a5df3ca99213ab8e3a9c6477cceae014
Martin Giger <martin@humanoids.be>
2019-01-24 00:15:06 +0100

    Update build system

--- build.sh
+++ build.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-rm -rf node_modules
-npm ci --production
-rm -f SHA256SUMS
-sha256sum package.json *.js LICENSE > SHA256SUMS
-find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+rm -rf *.tgz package/
 TARFILE=$(npm pack)
 tar xzf ${TARFILE}
+npm ci --ignore-scripts
+npm run licenses
+cd package
+sha256sum LICENSE > SHA256SUMS
+cd ..
+sha256sum package.json adapter.js property.js readonly-property.js speaker.js >> package/SHA256SUMS
+rm -rf node_modules
+npm ci --production --ignore-scripts
+find node_modules -type f -exec sha256sum {} \; >> package/SHA256SUMS
 cp -r node_modules ./package
 tar czf ${TARFILE} package
 rm -rf package
+sha256sum ${TARFILE}
 echo "Created ${TARFILE}"
